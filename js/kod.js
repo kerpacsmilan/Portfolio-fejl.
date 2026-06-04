e264581e37b8a3e808d66577da7ba4f608632391
kerpacsmilan <vasvillmilan@gmail.com>
2026-06-04 22:24:47 +0200

    Add files via upload

---
+++ js/kod.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            e.preventDefault();
+            window.scrollTo({ top: target.offsetTop - 60, behavior: 'smooth' });
+        }
+    });
+});
